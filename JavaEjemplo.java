19b24b5f8ca3c6cfd5c465fb56631b435e2603bb
AndersonOsbeli <andersonosbelitt555@gmail.com>
2025-04-02 17:45:04 -0600

    agregar opcion para seleccion de habitaciones

--- JavaEjemplo.java
+++ JavaEjemplo.java
@@ -42,5 +42,42 @@ public class JavaEjemplo {
                 \t%s
                 \t¡Felicidades!
                 """, nombre, idUnico);
+
+                System.out.println("precio de habitaciones");
+         
+         int estandar = 900;
+         int suite = 1500;
+         int premium = 2500;
+         
+         System.out.println("Habitacion estandar: "+ estandar);
+         System.out.println("Habitacion suite : "+ suite );
+         System.out.println("Habitacion premium: " + premium);
+         System.out.println("Seleccione el tipo de habitación que desea reservar:");
+         System.out.println("1. Estándar");
+         System.out.println("2. Suite");
+         System.out.println("3. Premium");
+         
+         int opcion = consola.nextInt();
+         int precioSeleccionado = 0;
+
+         switch (opcion) {
+             case 1:
+                 precioSeleccionado = estandar;
+                 System.out.println("Ha seleccionado una habitación Estándar.");
+                 break;
+             case 2:
+                 precioSeleccionado = suite;
+                 System.out.println("Ha seleccionado una habitación Suite.");
+                 break;
+             case 3:
+                 precioSeleccionado = premium;
+                 System.out.println("Ha seleccionado una habitación Premium.");
+                 break;
+             default:
+                 System.out.println("Opción no válida. Por favor, reinicie el programa e intente nuevamente.");
+                 return;
+         }
+
+         System.out.printf("El precio de la habitación seleccionada es: $%d%n", precioSeleccionado);
     }
 }
